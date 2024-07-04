5700f00088b5511dc5490389e8369fbdc22b524a
Abdullah S. Khan <abdullahshamskhan@gmail.com>
2024-07-04 11:46:54 -0400

    Configure program to handle calculations for differentiated payments in addition to annuity

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -2,6 +2,21 @@ from math import ceil, log
 import argparse
 
 
+def check_program_type(value):
+    if value not in ["annuity", "diff"]:
+        # raise argparse.ArgumentTypeError("Incorrect Parameters")
+        print('Incorrect Parameters')
+    return value
+
+
+def check_positive(value):
+    fl_value = float(value)
+    if fl_value <= 0.0:
+        # raise argparse.ArgumentTypeError("Incorrect Parameters")
+        print('Incorrect Parameters')
+    return fl_value
+
+
 def nominal_interest_rate(interest_rate):
     return interest_rate / (12 * 100)
 
@@ -28,7 +43,7 @@ def calculate_num_periods(principal, payment, interest):
     return ceil(num_months)
 
 
-def generate_message(years, months):
+def generate_annuity_message(years, months):
     message = "It will take"
 
     if years == 1:
@@ -45,27 +60,71 @@ def generate_message(years, months):
     return message
 
 
+def generate_diff_message(payments, overpayment):
+    message = ''
+
+    for i in range(len(payments)):
+        message += f'Month {i + 1}: payments is {payments[i]}\n'
+
+    message += f'\nOverpayment = {overpayment}'
+
+    return message
+
+
+def calculate_differentiated(principal, periods, interest):
+    i = nominal_interest_rate(interest)
+    payments = []
+
+    for m in range(1, int(periods) + 1):
+        D = principal / periods + i * (principal - (principal * (m - 1)) / periods)
+        payments.append(ceil(D))
+
+    return payments
+
+
 parser = argparse.ArgumentParser(
     prog='LoanCalculator',
     description='Personal Finance Loan Calculator',
 )
 
-parser.add_argument('--principal', type=float)
-parser.add_argument('--payment', type=float)
-parser.add_argument('--periods', type=float)
-parser.add_argument('--interest', type=float, required=True)
+parser.add_argument('--type', type=check_program_type)
+parser.add_argument('--principal', type=check_positive)
+parser.add_argument('--payment', type=check_positive)
+parser.add_argument('--periods', type=check_positive)
+parser.add_argument('--interest', type=check_positive)
 
 args = parser.parse_args()
-result = None
 
-if args.principal is None:
-    result = calculate_principal(args.payment, args.periods, args.interest)
-    print(f'Your loan principal = {result}!')
-elif args.payment is None:
-    result = calculate_annuity_payment(args.principal, args.periods, args.interest)
-    print(f'Your monthly payment = {result}!')
-elif args.periods is None:
-    result = calculate_num_periods(args.principal, args.payment, args.interest)
-    years, months = divmod(result, 12)
-    message = generate_message(years, months)
-    print(message)
+if args.type is None or args.interest is None:
+    print('Incorrect Parameters')
+elif args.type == "diff":
+    # raise error if all the right arguments are not provided
+    if not all([args.principal, args.periods, args.interest]):
+        # raise argparse.ArgumentTypeError("Incorrect Parameters")
+        print('Incorrect Parameters')
+    else:
+        payments = calculate_differentiated(args.principal, args.periods, args.interest)
+        overpayment = sum(payments) - int(args.principal)
+
+        message = generate_diff_message(payments, overpayment)
+        print(message)
+
+else:  # annuity
+    overpayment = 0
+
+    if args.principal is None:
+        loan_principal = calculate_principal(args.payment, args.periods, args.interest)
+        print(f'Your loan principal = {loan_principal}!')
+        overpayment = ceil(args.payment * args.periods - loan_principal)
+    elif args.payment is None:
+        ann_payment = calculate_annuity_payment(args.principal, args.periods, args.interest)
+        print(f'Your monthly payment = {ann_payment}!')
+        overpayment = ceil(ann_payment * args.periods - args.principal)
+    elif args.periods is None:
+        periods = calculate_num_periods(args.principal, args.payment, args.interest)
+        years, months = divmod(periods, 12)
+        message = generate_annuity_message(years, months)
+        print(message)
+        overpayment = ceil(args.payment * periods - args.principal)
+
+    print(f'Overpayment = {overpayment}')
